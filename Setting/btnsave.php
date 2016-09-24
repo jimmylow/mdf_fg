0e445940b99ce789f686f38c2f344b918d1192f2
jimmylow <jimmylow@wavelet.net>
2016-09-24 13:24:35 +0800

    Add confirm message before save

--- Setting/btnsave.php
+++ Setting/btnsave.php
@@ -2,6 +2,6 @@
   if ($var_accadd == 0){
    	echo '<input disabled="disabled" type=submit name = "Submit" value="Save" id="Save" class="butsub" style="width: 60px; height: 32px" >';
   }else{
-   	echo '<input type=submit name = "Submit" value="Save" class="butsub" id="Save" style="width: 60px; height: 32px" >';
+   	echo '<input type=submit name = "Submit" value="Save" class="butsub" id="Save" onclick="return confirm(\'Are you sure?\')" style="width: 60px; height: 32px" >';
   }
 ?>
